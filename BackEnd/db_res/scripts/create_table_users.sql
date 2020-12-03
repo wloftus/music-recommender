497e422ad5b02f2aafd3f85f29c700ab6f3b6c07
Will Loftus <willcaleloftus@gmail.com>
2020-12-03 07:22:54 -0800

    updated db scripts

--- BackEnd/db_res/scripts/create_table_users.sql
+++ BackEnd/db_res/scripts/create_table_users.sql
@@ -2,5 +2,6 @@ CREATE TABLE users (
     id          SERIAL PRIMARY KEY,
     username    TEXT NOT NULL,
     type        TEXT NOT NULL,
-    profile_img TEXT
+    profile_img TEXT,
+    UNIQUE(username),
 );
