1538e85c259a4bace6c185851cbef9b205acaba0
Will Loftus <willcaleloftus@gmail.com>
2020-12-03 06:06:23 -0800

    finished CRUD methods for users

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -7,7 +7,12 @@ const auth = require('../auth');
 // CREATE NEW USER (SIGNUP)
 // No authentication required because we need a user to before we get a token.
 router.post('/signup/username/:username/type/:type', (req, res) => {
-
+    db.query('INSERT INTO users(username, type) VALUES($1, $2)', [req.params.username, req.params.type], (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        res.send(results.rows[0]);
+    });
 });
 
 // GET USER (LOGIN)
@@ -21,14 +26,24 @@ router.get('/id/:id', (req, res) => {
     });
 });
 
-// UPDATE USER
-router.patch('/:user', auth.authenticateToken, (req, res) => {
-
+// UPDATE USER PROFILE IMG
+router.patch('/id/:id/profile_img/:profile_img', auth.authenticateToken, (req, res) => {
+    db.query('UPDATE users SET profile_img = $2 WHERE id = $1', [req.params.id, req.params.profile_img], (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        res.send(results.rows[0]);
+    });
 });
 
 // DELETE USER
 router.delete('/id/:id', auth.authenticateToken, (req, res) => {
-
+    db.query('DELETE FROM users WHERE id = $1', [req.params.id], (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        res.send(results.rows[0]);
+    });
 });
 
 module.exports = router;
