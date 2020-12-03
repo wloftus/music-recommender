f6da00acd05ac34d686e267a622d8fd8f577ff10
Will Loftus <willcaleloftus@gmail.com>
2020-12-03 04:59:24 -0800

    added cors policy

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors')
 
 const port = process.env.PORT || 3000;
 
@@ -7,6 +8,8 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 dotenv.config();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({limit: '5mb', extended: false }));
