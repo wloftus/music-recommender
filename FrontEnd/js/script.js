a63b48810f3e3f2aea62de41e4edf4d781e0a81e
Colleen Lazo <lazocolleen@gmail.com>
2020-12-02 21:29:00 -0800

    Setting up JS strings and the email validator

---
+++ FrontEnd/js/script.js
@@ -0,0 +1,4 @@
+validateEmail(() => {  // or make this an onlclick event using jquery and => event
+    // Use Regex to validate the email
+    let email = $(EMAIL_ID_SEL).val();
+});
---
+++ FrontEnd/js/strings.js
@@ -0,0 +1,3 @@
+const EMAIL_ID_SEL = "#email";
+const FIRST_NAME_ID_SEL = "#first-name";
+const LAST_NAME_ID_SEL = "#last-name";
