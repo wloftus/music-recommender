39d0f8db541946ec71e48b53140fa67a5b088c56
Will Loftus <willcaleloftus@gmail.com>
2020-12-04 12:57:13 -0800

    fixed doc and styled

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -13,6 +13,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({limit: '5mb', extended: false }));
+app.use(express.static('public'))
 
 app.use('/api/v1/', indexRouter);
 
