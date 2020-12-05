c1523d7cfc736365640db2be628b3c4dc78140f6
Colleen Lazo <lazocolleen@gmail.com>
2020-12-04 20:33:16 -0800

    added function to get username, added function for images suign dataURL

--- FrontEnd/js/strings.js
+++ FrontEnd/js/strings.js
@@ -1,3 +1,6 @@
-const EMAIL_ID_SEL = "#email";
-const FIRST_NAME_ID_SEL = "#first-name";
-const LAST_NAME_ID_SEL = "#last-name";
+
+const USERNAME_ID_SEL = "#username";
+const USERNAME_BUTTON_ID_SEL = "#submit-username";
+
+const USER_IMG_ID_SEL = "user-img";
+const IMG_FILE_TYPE_SEL = "input[type=file]";
