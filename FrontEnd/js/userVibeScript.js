c9823a7ec60f13d7d24f7044f39bcefb69f3e9d5
Colleen Lazo <lazocolleen@gmail.com>
2020-12-05 18:33:54 -0800

    debugging userVibeScript.js

--- FrontEnd/js/userVibeScript.js
+++ FrontEnd/js/userVibeScript.js
@@ -1,30 +1,25 @@
 
 // Called in the index.html when the slider is moved
 function updateTextInput(vibeScore) {
-    console.log(vibeScore); 
     onUpdateVibe(vibeScore);
 }
 
 // On click listener
 function onUpdateVibe(vibeScore) {
     getUserVibeByScore(vibeScore);
-    let vibeName = localStorage.getItem('vibe_name');
-    $(USER_VIBE_RESULT_ID_SEL).text(vibeName); 
 }
 
 function getUserVibeByScoreListener() {
-    console.log("This status: " + this.status);
-    console.log("This response: " + this.responseText);
     // Creation successful
     if (this.status === 200) {
         let vibe_name = JSON.parse(this.responseText)[0]['name'];
         let vibe_score = JSON.parse(this.responseText)[0]['score'];
 
-        console.log("vibe_name: " + vibe_name);
-        console.log("vibe_score: " + vibe_score);
-
         localStorage.setItem('vibe_name', vibe_name);
         localStorage.setItem('vibe_score', vibe_score);
+
+        // Needs to be here to update right away
+        $(USER_VIBE_RESULT_ID_SEL).text(vibe_name);
 
     } 
     // Failed, only if the user somehow enters something not within
@@ -34,7 +29,6 @@ function getUserVibeByScoreListener() {
     }
 }
 
-
 function getUserVibeByScore(score) {
     // Make a request
     let oReq = new XMLHttpRequest();
@@ -42,10 +36,10 @@ function getUserVibeByScore(score) {
     oReq.open("GET", "https://kanyemusicrecommender.herokuapp.com/api/v1/vibes/score/" + score);
 
     // Add authorization
-    // let token = localStorage.getItem('token');
+    let token = localStorage.getItem('token');
 
     // For testing
-    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDcwOTYzNzJ9.UaVT23_Fb3wEksk4AqDNYMBoDi-hw0iwFAkJH3oRXyA";
+    // let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2MDcwOTYzNzJ9.UaVT23_Fb3wEksk4AqDNYMBoDi-hw0iwFAkJH3oRXyA";
     oReq.setRequestHeader("authorization", token);
     oReq.send();
 }
