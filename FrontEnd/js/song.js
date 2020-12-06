9d84a7d398111b8da777b06406f2acf39f09e658
Colleen Lazo <lazocolleen@gmail.com>
2020-12-05 20:56:58 -0800

    Cleaning

--- FrontEnd/js/song.js
+++ FrontEnd/js/song.js
@@ -4,44 +4,12 @@ $(SONG_SUBMIT_BTN_ID_SEL).click(function() {
     console.log("In jQuery function.")
     let vibeScore = localStorage.getItem('vibe_score')
     onUpdateSongVibe(vibeScore);
-
-    // console.log(localStorage.getItem('song_name'));
-    // console.log(localStorage.getItem('album_name'));
-    // console.log(localStorage.getItem('song_link'));
-    // console.log(localStorage.getItem('img'));
 });
-
 
 // On click listener
 function onUpdateSongVibe(vibeScore) {
     getSongVibeByScore(vibeScore);
 }
-
-function getMultipleSongs(jsonObj) {
-    // for each json obj in the response, get the songs?
-}
-
-// Preview the image file 
-// function previewFile(imgLink) {
-//     const preview = document.querySelector(SONG_IMG_ID_SEL);
-//     //const file = document.querySelector(IMG_FILE_TYPE_SEL).files[0];
-//     const file = imgLink;
-//     const reader = new FileReader();
-  
-//     reader.addEventListener("load", function () {
-//       // convert image file to base64 string
-//       let imgString = reader.result;
-//       preview.src = imgString;
-//     }, false);
-  
-//     if (file) {
-//       reader.readAsDataURL(file);
-//     }
-//     else {
-//         alert("This is not an image.");
-//     }
-//     // clear local storage?
-// }
 
 function getSongVibeByScoreListener() {
     // Creation successful
@@ -51,23 +19,10 @@ function getSongVibeByScoreListener() {
         let song_link = JSON.parse(this.responseText)[0]['song_link'];
         let img = JSON.parse(this.responseText)[0]['img']
 
-        // Is there a way to make use of these?
-        // localStorage.setItem('song_name', song_name);
-        // localStorage.setItem('album_name', album_name);
-        // localStorage.setItem('song_link', song_link);
-        // localStorage.setItem('img', img);
-
         $(SONG_TITLE_ID_SEL).text(song_name);
         $(SONG_ALBUM_ID_SEL).text(album_name);
         $(SONG_LINK_ID_SEL).attr("href", song_link);
         $(SONG_IMG_ID_SEL).attr("src", img);
-        //previewFile(img);
-
-        // console.log("In getSongVibeByScoreListener function.")
-        // console.log(localStorage.getItem('song_name'));
-        // console.log(localStorage.getItem('album_name'));
-        // console.log(localStorage.getItem('song_link'));
-        // console.log(localStorage.getItem('img'));   
     } 
     // Failed, only if the user somehow enters something not within
     // the range of 1 to 10.
